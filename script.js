9bd89323d625fc6c8fe1d694adc0e3587fe89d14
Equator919 <143641264+Equator919@users.noreply.github.com>
2025-05-21 23:08:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ signInButton.addEventListener('click', () => {
 document.getElementById("bt1").addEventListener("click", function() {
   this.style.display = "none";
 });
+const button = document.getElementById('colorButton');
+
+const colors = ['#3498db', '#e74c3c', '#2ecc71', '#9b59b6', '#f1c40f', '#1abc9c', '#e67e22'];
+let colorIndex = 0;
+
+button.addEventListener('click', () => {
+  colorIndex = (colorIndex + 1) % colors.length;
+  button.style.backgroundColor = colors[colorIndex];
+});
